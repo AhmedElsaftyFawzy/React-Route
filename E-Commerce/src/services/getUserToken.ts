fbceda09bff6ccd3a259abcbe11867449212be7c
AhmedElsaftyFawzy <113257878+AhmedElsaftyFawzy@users.noreply.github.com>
2025-09-11 18:27:15 +0300

    Fixing GetUserToken

--- E-Commerce/src/services/getUserToken.ts
+++ E-Commerce/src/services/getUserToken.ts
@@ -5,8 +5,9 @@ import { cookies } from "next/headers"
 export async function getUserToken() {
   const tokenSession =
     process.env.NODE_ENV == "production"
-      ? "_Secure-next-auth.session-token"
+      ? "__Secure-next-auth.session-token"
       : "next-auth.session-token"
+
   const cookie = (await cookies()).get(tokenSession)?.value
   const data = await decode({
     token: cookie,
