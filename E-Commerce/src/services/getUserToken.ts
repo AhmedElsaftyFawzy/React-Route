ba239a26681667699eaf6756aae154adb3bf8100
AhmedElsaftyFawzy <113257878+AhmedElsaftyFawzy@users.noreply.github.com>
2025-09-11 18:15:13 +0300

    Change GetUserToken

--- E-Commerce/src/services/getUserToken.ts
+++ E-Commerce/src/services/getUserToken.ts
@@ -3,7 +3,11 @@ import { decode } from "next-auth/jwt"
 import { cookies } from "next/headers"
 
 export async function getUserToken() {
-  const cookie = (await cookies()).get("next-auth.session-token")?.value
+  const tokenSession =
+    process.env.NODE_ENV == "production"
+      ? "_Secure-next-auth.session-token"
+      : "next-auth.session-token"
+  const cookie = (await cookies()).get(tokenSession)?.value
   const data = await decode({
     token: cookie,
     secret: process.env.AUTH_SECRET!,
