cfe5bcd4394e75f4e1a8b640fe2ba0b8e91a1580
AhmedElsaftyFawzy <113257878+AhmedElsaftyFawzy@users.noreply.github.com>
2025-09-12 00:01:49 +0300

    Change SubCategory Page

--- E-Commerce/src/app/(main)/category/[id]/page.tsx
+++ E-Commerce/src/app/(main)/category/[id]/page.tsx
@@ -11,12 +11,12 @@ export default async function page({ params }: { params: { id: string } }) {
   const id = await params.id
   const res: CategoryData = await getAllSubCategories(id)
   return (
-    <div className="grid grid-cols-3 m:grid-col-6 lg:grid-col-12 gap-5 my-5 px-5">
+    <div className="grid grid-cols-12 gap-5 my-5 px-5">
       {res?.data.map((category) => {
         return (
           <Card
             key={category._id}
-            className="relative col-span-3 rounded-4xl h-50 overflow-hidden"
+            className="relative lg:col-span-2 md:col-span-4 sm:col-span-6 col-span-12 rounded-4xl h-50 overflow-hidden"
           >
             <div className="absolute inset-0 bg-black/50 flex justify-center items-center">
               <h2 className="text-white text-lg font-semibold">
